765ba8b6084a6efdfb8e3332c07a617c750b0d3f
Enriquecerviro <enrique.cerviro@gmail.com>
2019-12-30 17:42:55 +0100

    error en la configuracion del Startup.cs

---
+++ ControllersAndActions/Controllers/PocoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ControllersAndActions.Controllers
+{
+    public class PocoController : Controller
+    {
+        // GET: /<controller>/
+        public string Index() => "This is a POCO controller";
+       
+    }
+}
